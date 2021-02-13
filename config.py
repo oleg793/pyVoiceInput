6f01205dc4deeb11770146b20ee037e9ec9cbab4
oleg793 <70800784+oleg793@users.noreply.github.com>
2021-02-13 15:42:10 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+select_all = "select all"
+
+backspace = "delete"
+
+exit = "exit"
+
+dot = "complete"
+
+save = "save"
+
+open_editor = "edit text"
+editor = "notepad.exe"
+
+close = "close"
+
+lang = "en-En"
